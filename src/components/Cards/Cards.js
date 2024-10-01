c62b9a774d1dd76af9aeb7a784ca6c0713899399
Mejri Mohamed Aziz <aziz30mejri@gmail.com>
2024-10-01 11:57:14 +0100

    Engagement initial

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -3,16 +3,16 @@ import {FaStar}from 'react-icons/fa';
 import {BiCloudDownload} from 'react-icons/bi';
 
 const Cards=(props)=> {
-    
     return(
-        <div style={{display:"inline-block",flexWrap:"wrap",justifyContent:"space-between",marginLeft:"40px",marginTop:"10px"}}>
+        <div style={{display:"inline-block",flexWrap:"wrap",justifyContent:"space-between",marginLeft:"55px",marginTop:"10px"}}>
             <div className="card" style={{width: "18rem",marginLeft:"20px"}}>
-            <img src={props.img} className="card-img-top" alt="..."style={{width: "15rem",marginLeft:"25px",maxHeight:"320px",marginTop:"25px"}}/>
-            <div className="card-body">
-            <h4 className="card-title"style={{textAlign:"center"}}>{props.nom}</h4>
-            <h6 className="card-telechargement"style={{textAlign:"center"}}>{props.notation}   <FaStar/> </h6>
-            <h6 className="card-telechargement"style={{textAlign:"center"}}>{props.telechargement}   <BiCloudDownload/> </h6>
-             </div>
+                <img src={props.img} className="card-img-top zoom-image" alt="..."
+                style={{width: "15rem",marginLeft:"25px",maxHeight:"320px",marginTop:"25px",cursor:"pointer"}}/>
+                <div className="card-body">
+                    <h4 className="card-title"style={{textAlign:"center"}}>{props.nom}</h4>
+                    <h6 className="card-telechargement"style={{textAlign:"center"}}>{props.notation} <FaStar className="star-icon" style={{marginTop:"-4px"}}/> </h6>
+                    <h6 className="card-telechargement"style={{textAlign:"center"}}>{props.telechargement} <BiCloudDownload className="download-icon"/> </h6>
+                </div>
             </div>
         </div>
     )
