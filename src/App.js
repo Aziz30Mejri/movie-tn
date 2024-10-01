c0afa008164f1fb84e488fb5af0451ce2821cfb0
Mejri Mohamed Aziz <aziz30mejri@gmail.com>
2024-10-01 11:54:15 +0100

    Engagement initial

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import React from'react';
+import React from 'react';
 import './App.css';
-import Header from './components/Header';
 import Accueil from './components/Pages/Accueil/Accueil';
 import { BrowserRouter as Router,Route,Routes } from 'react-router-dom';
 import Series from './components/Pages/Series/Series';
@@ -8,25 +7,18 @@ import Films from './components/Pages/Films/Films';
 import Footer from './components/Footer';
 import Contact from './components/Pages/Contact/Contact';
 
-
 function App() {
     return(
       <Router>
-      
-        <Header/>
-        
         <Routes>
         <Route path='/' element={<Accueil/>}/>
         <Route path='/series' element={<Series/>}/>
         <Route path='/films' element={<Films/>}/>
         <Route path='/contact' element={<Contact/>}/>
         </Routes>
-
         <Footer/>
-
       </Router>
     );
 }
 
 export default App;
-
