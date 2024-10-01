ecbfd1e1fa907ec781695f6ca38545dbb13b292b
Mejri Mohamed Aziz <aziz30mejri@gmail.com>
2024-10-01 11:56:16 +0100

    Engagement initial

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,45 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { NavLink, useLocation } from 'react-router-dom';
 
-const titre = "Movie Tn"
-const Acc = "Accueil"
-const Ser = "Séries"
-const Fil = "Films"
-const Con = "Contact"
-const search = "Rechercher"
+const titre = "Movie Tn";
+const Acc = "Accueil";
+const Ser = "Séries";
+const Fil = "Films";
+const Con = "Contact";
 
-const Header = () =>{
-    return(
-      <nav className="navbar navbar-expand-lg bg-body-tertiary" style={{ position: "sticky", top: 0, zIndex: 100 }}> 
-  <div className="container-fluid">
-    <img src="images/icon.png" alt="Logo" width="23" height="23" className="d-inline-block align-text-top"/> 
-    <a className="navbar-brand" style={{paddingLeft:"7px"}} href="/">{titre}</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
+const Header = ({ onSearch }) => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+    onSearch(event.target.value);
+  };
+  const location = useLocation();
+  const isHomePage = location.pathname === '/';
+  return (
+    <nav className="navbar navbar-expand-lg bg-body-tertiary" style={{ position: "sticky", top: 0, zIndex: 100 }}>
+      <div className="container-fluid">
+        <img src="images/icon.png" alt="Logo" width="23" height="23" className="d-inline-block align-text-top" />
+        <NavLink className="navbar-brand" style={{ paddingLeft: "7px" }} to="/">{titre}</NavLink>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <NavLink className="nav-link" exact to="/" activeClassName="active">{Acc}</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/series" activeClassName="active">{Ser}</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/films" activeClassName="active">{Fil}</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/contact" activeClassName="active">{Con}</NavLink>
+            </li>
+          </ul>
+          <form className="d-flex" role="search">
+            <input className="form-control me-2"
+            type="search"
+            placeholder="Rechercher" 
+            aria-label="Search"
+            value={searchTerm}
+            onChange={handleSearchChange} 
+            disabled={isHomePage}
+              style={{cursor: isHomePage ? 'not-allowed' : 'text',}}/>
+            <button className="btn btn-outline-success"type="button"
+            disabled={isHomePage}
+            style={{cursor: isHomePage ? 'not-allowed' : 'pointer',}}
+            >Rechercher</button>
+          </form>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/">{Acc}</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/series">{Ser}</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/films">{Fil}</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/contact">{Con}</a>
-        </li>
-      </ul>
-
-      <form className="d-flex" role="search">
-        <input className="form-control me-2" type="search" placeholder="Rechercher" aria-label="Search" />
-        <button className="btn btn-outline-success" type="submit">{search}</button>
-      </form>
-    </div>
-  </div>
-</nav>
-        )
-  }
 export default Header;
