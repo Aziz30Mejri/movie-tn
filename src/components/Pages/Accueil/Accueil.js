7e1509eec78a5726c6c0685b5af960e09c400614
Mejri Mohamed Aziz <aziz30mejri@gmail.com>
2024-10-01 11:58:47 +0100

    Engagement initial

--- src/components/Pages/Accueil/Accueil.js
+++ src/components/Pages/Accueil/Accueil.js
@@ -1,29 +1,29 @@
 import React from 'react';
 import Slider from '../../Slider';
+import Header from '../../Header';
 import ListCards2 from '../../Cards/ListCards2';
 import ListCards from '../../Cards/ListCards';
 import {RiSlideshow3Fill} from 'react-icons/ri' ;
 import {BiCameraMovie} from 'react-icons/bi';
 
-const StyleCss = {marginBottom:"30px",marginTop:"30mpx"}
-
 const Accueil = () => {
   return (
     <React.Fragment>
-    <Slider/>
+      <Header/>
+      <Slider/>
         <h3 style={{textAlign:"center",marginTop:"30px",marginBottom:"30px",color:"white"}}>Tendance Série Tv <RiSlideshow3Fill/> </h3>
         <ListCards/>
         <div className="container" style={{marginTop:"25px"}}>
-          <a href='/series' className='button' style={{textDecoration:"none",width:"190px"}}>Lire Plus !</a>
+          <a href='/series' className='button' style={{textDecoration:"none",width:"190px"}}>
+          <span> Lire Plus ! </span></a>
         </div>
         <h3 style={{textAlign:"center",marginTop:"30px",marginBottom:"30px",color:"white"}}>Tendance Films <BiCameraMovie/> </h3>
         <ListCards2/>
         <div className="container" style={{marginTop:"25px",marginBottom:"30px"}}>
-          <a href='/films' className='button' style={{textDecoration:"none",width:"190px"}}>Lire Plus !</a>
+          <a href='/films' className='button' style={{textDecoration:"none",width:"190px"}}>
+           <span> Lire Plus ! </span></a>
         </div>
     </React.Fragment>
-    
   );
 }
-
 export default Accueil;
